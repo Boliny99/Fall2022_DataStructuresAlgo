ccc0d8c0f447a56b1c157455da200a7e89871b1e
Boliny99 <yang.bol@northeastern.edu>
2022-12-11 11:44:49 -0800

    checking out code for q1 add complexity

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Main {
+
+    //Time comlexity : O(n)
+    //Space complexity: O(n)
     public static void main(String[] args) {
         String[] strs = {"eat","tea","tan","ate","nat","bat"};
         List<List<String>> res = groupAnagrams(strs);
@@ -14,12 +17,13 @@ public class Main {
         for (String str : strs) {
             char[] ch = str.toCharArray();
             Arrays.sort(ch);
-            if (!map.containsKey(ch.toString())) {
-                map.put(ch.toString(),new ArrayList<>());
+            String key = String.valueOf(ch);
+            if (!map.containsKey(key)) {
+                map.put(key,new ArrayList<>());
             }
-            List<String> val = map.get(ch.toString());
+            List<String> val = map.get(key);
             val.add(str);
-            map.put(ch.toString(),val);
+            map.put(key,val);
         }
         res = new ArrayList<>(map.values());
         return res;
