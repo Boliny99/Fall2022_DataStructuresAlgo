d16a83d240289ba033943f6fefd72d8c72ba0135
Boliny99 <yang.bol@northeastern.edu>
2022-12-10 19:58:06 -0800

    checking in code for Longest Substring without repeating

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "wwwke";
+        System.out.println(longestNonRepeatingSubstring(s));
+    }
+
+
+    private static String longestNonRepeatingSubstring(String str) {
+        if (str.length() <= 1) return str;
+        HashSet<Character> set = new HashSet<>();
+        int start = 0;
+        int maxLength = 0;
+        String maxNonRepeated ="";
+
+        for (int i = 0;i < str.length();i++) {
+            if(!set.contains(str.charAt(i))) {
+                set.add(str.charAt(i));
+                if (maxLength < i -start + 1) {
+                    maxLength = i - start + 1;
+                    maxNonRepeated = str.substring(start,i+1);
+                }
+            } else {
+                while (set.contains(str.charAt(i))) {
+                    set.remove((str.charAt(start)));
+                    start++;
+                }
+                set.add(str.charAt(i));
+            }
+        }
+        return maxNonRepeated;
+    }
+}
